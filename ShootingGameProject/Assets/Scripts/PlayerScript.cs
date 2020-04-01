c193219d7e0a302ed35159ca715bfee0b53a5fe2
yukikokoa <rei22.lucky.tennis@icloud.com>
2020-04-01 12:02:12 +0900

    [update]飛行機動くようになった

---
+++ ShootingGameProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = Input.GetAxis("Horizontal") * Time.deltaTime * 8f;
+        float dy = Input.GetAxis("Vertical") * Time.deltaTime * 8f;
+
+        transform.position = new Vector3(
+            transform.position.x + dx,
+            transform.position.y + dy);
+    }
+}
