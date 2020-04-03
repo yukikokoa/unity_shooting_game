983be4f6b4b911aa5018c24cf1a0de5e115d86ff
yukikokoa <rei22.lucky.tennis@icloud.com>
2020-04-03 16:09:44 +0900

    [Add]CompleteShootingGame

--- ShootingGameProject/Assets/Scripts/EnemyScript.cs
+++ ShootingGameProject/Assets/Scripts/EnemyScript.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    private float phase;
+    public GameObject explosion;
+    private GameControllerScript gameController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        phase = Random.Range(0f, Mathf.PI * 2);
+        gameController = GameObject
+            .FindWithTag("GameController")
+            .GetComponent<GameControllerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0f, -2f * Time.deltaTime, 0f);
+        transform.position += new Vector3(
+            Mathf.Cos(Time.frameCount * 0.05f + phase) * 0.05f,
+            -2f * Time.deltaTime, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            gameController.AddScored(10);
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameController.GameOver();
+            Instantiate(explosion, transform.position, transform.rotation);
+            Instantiate(explosion, collision.transform.position, transform.rotation);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
--- ShootingGameProject/Assets/Scripts/GameControllerScript.cs
+++ ShootingGameProject/Assets/Scripts/GameControllerScript.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameControllerScript : MonoBehaviour
 {
     public GameObject enemy;
+    private int score;
+    public Text scoreText;
+    public Text replayText;
+    private bool isGameOver;
 
     IEnumerator SpawnEnemy()
     {
@@ -24,11 +31,42 @@ public class GameControllerScript : MonoBehaviour
     void Start()
     {
         StartCoroutine("SpawnEnemy");
+        score = 0;
+        UpdateScoreText();
+        replayText.text = "";
+        isGameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Spaceキーを押したらリプレイされるのを防ぐ。
+        if(isGameOver == false)
+        {
+            return;
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    public void AddScored(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        replayText.text = "Hit SPACE to replay!";
     }
 }
+
